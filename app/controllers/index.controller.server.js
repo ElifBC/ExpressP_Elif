57b1c827c17a8cfaaf67ddf7117f7aa1dab3077e
Elif Bilge Canatan <ebilgeca@my.centennialcollege.ca>
2022-10-06 23:58:44 -0400

    Portfolio ontent added

--- app/controllers/index.controller.server.js
+++ app/controllers/index.controller.server.js
@@ -1,5 +1,25 @@
 // Index Controller
 
-export function helloWorld(req, res, next){
-    res.render('index', {title: 'Home'});
+export function displayHomePage(req, res, next){
+    res.render('index', {title: 'Home', page: 'home'});
+
+};
+
+export function displayAboutPage(req, res, next){
+    res.render('index', {title: 'About', page: 'about'});
+};
+
+export function displayContactPage(req, res, next){
+    res.render('index', {title: 'Contact', page: 'contact'});
+
+};
+
+export function displayHobbiesPage(req, res, next){
+    res.render('index', {title: 'Hobbies', page: 'hobbies'});
+
+};
+
+export function displayProjectsPage(req, res, next){
+    res.render('index', {title: 'Projects', page: 'projects'});
+
 };
--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,13 +1,17 @@
 // Index Routes
 import { Router } from 'express';
-import { helloWorld } from '../controllers/index.controller.server.js'
+import { displayHomePage, displayAboutPage, displayContactPage, displayProjectsPage, displayHobbiesPage } from '../controllers/index.controller.server.js'
 
 //Instanciating the router
 const router = Router();
 
 
 // add middleware to connect application
-router.get('/',helloWorld);
-router.get('/hello',helloWorld);
+router.get('/',displayHomePage);
+router.get('/hello',displayHomePage);
+router.get('/about', displayAboutPage);
+router.get('/contact', displayContactPage);
+router.get('/projects', displayProjectsPage);
+router.get('/services', displayHobbiesPage);
 
 export default router;
